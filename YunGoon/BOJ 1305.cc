e1f80441488c6a52e176addb13f85803252a9858
YunGoon <dsyun96@naver.com>
2018-11-15 21:21:45 +0900

    오타 수정

--- YunGoon/BOJ 1305.cc
+++ YunGoon/BOJ 1305.cc
@@ -7,7 +7,7 @@ int main() {
 	int n;
 	scanf("%d %s", &n, str);
 
-	// 그냥 KMP 실패함수를 구현하는 끝나는 매우 간단한 문제.
+	// 그냥 KMP 실패함수를 구현하면 끝나는 매우 간단한 문제.
 	// KMP를 사용해야 한다는 것을 깨닫는 것이 포인트.
 	for (int i = 1, j = 0; str[i]; ++i) {
 		while (j > 0 && str[j] != str[i]) j = fail[j - 1];
